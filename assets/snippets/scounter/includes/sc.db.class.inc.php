75940e0931b4487086a8486d704a657683e7ad38
yama <yamamoto@kyms.jp>
2021-05-25 18:46:53 +0900

    refactor

--- assets/snippets/scounter/includes/sc.db.class.inc.php
+++ assets/snippets/scounter/includes/sc.db.class.inc.php
@@ -1,109 +1,121 @@
 <?php
-class SCounterDataDb {
-	var $ip;	//the users ip
-	var $time;	//the time now
-	var $tbl = array();
-	var $total;
-	var $today;
-	var $yesterday;
-	var $useronline;
-	
-	function SCounterDataDb() {
+class SCounterDataDb
+{
+	private $ip;	//the users ip
+	private $tbl = array();
+	private $total;
+	private $today;
+	private $yesterday;
+	private $useronline;
+
+	function __construct()
+	{
 		global $modx;
-		$this->tbl["check"]		 = $GLOBALS['table_prefix']."scounter_useronline";
-		$this->tbl["useronline"] = $modx->getFullTableName("scounter_useronline");
-		$this->tbl["daycount"]	 =   $modx->getFullTableName('scounter_daycount');
+		$this->tbl['check']		 = $GLOBALS['table_prefix'] . "scounter_useronline";
+		$this->tbl['useronline'] = $modx->getFullTableName("scounter_useronline");
+		$this->tbl['daycount']	 =   $modx->getFullTableName('scounter_daycount');
 		$this->ip	= $_SERVER['REMOTE_ADDR'];
 	}
-	
-	function FirstRun($path) {
-		global $modx;
-		$rs = $modx->db->query("SHOW TABLES LIKE '".$this->tbl["check"]."'");
-		$count = $modx->db->getRecordCount($rs);
-		
-		if ($count==0) {
-			$fh = fopen($path."includes/sc.install.db.sql", 'r');
-			$idata = '';
-			while (!feof($fh)) {
-				$idata .= fread($fh, 1024);
-			}
-			fclose($fh);
-			$idata = str_replace("\r", '', $idata);
-			$idata = str_replace('{PREFIX}',$GLOBALS['table_prefix'], $idata);
-			$sql_array = split("\n\n", $idata);
-			foreach($sql_array as $sql_entry) {
-				$sql_do = trim($sql_entry, "\r\n; ");
-				$modx->db->query($sql_do);	
-			}
+
+	function FirstRun($path)
+	{
+		$rs = db()->query("SHOW TABLES LIKE '" . $this->tbl["check"] . "'");
+		$count = db()->getRecordCount($rs);
+
+		if ($count != 0) {
+			return;
+		}
+
+		$fh = fopen($path . "includes/sc.install.db.sql", 'rb');
+		$idata = '';
+		while (!feof($fh)) {
+			$idata .= fread($fh, 1024);
+		}
+		fclose($fh);
+		$idata = str_replace("\r", '', $idata);
+		$idata = str_replace('{PREFIX}', $GLOBALS['table_prefix'], $idata);
+		$sql_array = explode("\n\n", $idata);
+		foreach ($sql_array as $sql_entry) {
+			$sql_do = trim($sql_entry, "\r\n; ");
+			db()->query($sql_do);
 		}
 	}
-	
-	function Read($time) {
-		global $modx;
+
+	function Read($time)
+	{
 		$this->Delete($time);
-		$rs = $modx->db->select(" * ", $this->tbl["useronline"], '`ip` = "'.$this->ip.'"');
-		$count = $modx->db->getRecordCount($rs);
-		if ($count == 1)	$return = $this->Update();
-		else				$return = $this->Writenew();
-		
+		$rs = db()->select(" * ", $this->tbl["useronline"], '`ip` = "' . $this->ip . '"');
+		$count = db()->getRecordCount($rs);
+		if ($count == 1) {
+			$this->Update();
+		} else {
+			$this->Writenew();
+		}
+
 		$this->ReadDate();
 		return array($this->total, $this->today, $this->yesterday, $this->useronline);
 	}
-	
-	function Writenew() {
-		global $modx;
+
+	function Writenew()
+	{
 		//create row for new user-online
-		$modx->db->query('INSERT INTO '.$this->tbl["useronline"].' VALUES ("'.$this->ip.'", NOW())');
-		
+		db()->query(
+			'INSERT INTO ' . $this->tbl["useronline"] . ' VALUES ("' . $this->ip . '", NOW())'
+		);
+
 		// write into <today> one new user
-		$rs = $modx->db->select(" * ", $this->tbl["daycount"], '`date` = CURDATE( )');
-		if($modx->db->getRecordCount($rs) == 1) {
-			while($row = $modx->db->getRow($rs)) {
+		$rs = db()->select(" * ", $this->tbl["daycount"], '`date` = CURDATE( )');
+		if (db()->getRecordCount($rs) == 1) {
+			while ($row = db()->getRow($rs)) {
 				$count = $row['count'];
 			}
 			$count++;
-			$modx->db->update("`count` = '".$count."'", $this->tbl["daycount"], "`date` = CURDATE()");
+			db()->update("`count` = '" . $count . "'", $this->tbl["daycount"], "`date` = CURDATE()");
 		}
 	}
-	
-	function Update() {
-		global $modx;
+
+	function Update()
+	{
 		//update the time in the database, if user is still online
-		$modx->db->update("`time` = NOW()", $this->tbl["useronline"], '`ip` = "'. $this->ip .'"');
+		db()->update("`time` = NOW()", $this->tbl["useronline"], '`ip` = "' . $this->ip . '"');
 	}
-	
-	function Delete($time) {
-		global $modx;
+
+	function Delete($time)
+	{
 		//create all rows which are older than $time minutes
-		$modx->db->query('DELETE FROM '.$this->tbl["useronline"].' WHERE DATE_SUB(NOW(), INTERVAL '.$time.' MINUTE) > time');
+		db()->query(
+            sprintf(
+                'DELETE FROM %s WHERE DATE_SUB(NOW(), INTERVAL %s MINUTE) > time',
+                $this->tbl["useronline"],
+                $time
+            )
+		);
 	}
-	
-	function ReadDate() {
-		global $modx;
+
+	function ReadDate()
+	{
 		// Read Users from today - if row not exists - create it.
-		$rs = $modx->db->select(" * ", $this->tbl["daycount"], '`date` = CURDATE( )');
-		if($modx->db->getRecordCount($rs) == 1) {
-			while($row = $modx->db->getRow($rs)) {
+		$rs = db()->select(" * ", $this->tbl["daycount"], '`date` = CURDATE( )');
+		if (db()->getRecordCount($rs) == 1) {
+			while ($row = db()->getRow($rs)) {
 				$this->today = $row['count'];
 			}
-		}
-		else {
-			$modx->db->query("INSERT INTO ".$this->tbl["daycount"]." VALUES (CURDATE(), '1')");
+		} else {
+			db()->query("INSERT INTO " . $this->tbl["daycount"] . " VALUES (CURDATE(), '1')");
 			$this->today = 1;
 		}
 		// Read Users from yesterday
-		$rs = $modx->db->select(" * ", $this->tbl["daycount"], '`date` = DATE_SUB(CURDATE( ), INTERVAL 1 DAY)');
-		while($row = $modx->db->getRow($rs)) {
+		$rs = db()->select(" * ", $this->tbl["daycount"], '`date` = DATE_SUB(CURDATE( ), INTERVAL 1 DAY)');
+		while ($row = db()->getRow($rs)) {
 			$this->yesterday = $row['count'];
 		}
-		if ($this->yesterday == "") $this->yesterday =0;
+		if ($this->yesterday == "") $this->yesterday = 0;
 		// Read Users from totals
-		$rs = $modx->db->select(" SUM(count) ", $this->tbl["daycount"]);
-		while($row = $modx->db->getRow($rs)) {
+		$rs = db()->select(" SUM(count) ", $this->tbl["daycount"]);
+		while ($row = db()->getRow($rs)) {
 			$this->total = $row['SUM(count)'];
 		}
-		$rs = $modx->db->select(" * ", $this->tbl["useronline"]);
-		$this->useronline = $modx->db->getRecordCount($rs);
+		$rs = db()->select(" * ", $this->tbl["useronline"]);
+		$this->useronline = db()->getRecordCount($rs);
 	}
 }
-?>
--- assets/snippets/scounter/sc.class.inc.php
+++ assets/snippets/scounter/sc.class.inc.php
@@ -10,60 +10,65 @@
 #
 ####*/
 
-class SCounter {
-	var $name;
-	var $version;
-	var $config = array();
-	var $output = array();
-	var $_ctime;
-	
-	
-	function SCounter() {
-		global $modx;
-		$path = strtr(realpath(dirname(__FILE__)), '\\', '/');
-		include_once($path . '/includes/sc.db.class.inc.php');
+class SCounter
+{
+	private $version;
+	private $config = array();
+	private $output = array();
+
+	function __construct()
+	{
+		include_once(__DIR__ . '/includes/sc.db.class.inc.php');
 		$this->database = new SCounterDataDb;
-		$this->name = $this->config["snippet"]["name"] = "SCounter";
-		$this->version = $this->config["snippet"]["version"] = "1.3";
-		$this->config["snippet"]["versioncheck"] = "Unknown";
-		$this->client = $modx->getUserData();
+		$this->name = $this->config['snippet']['name'] = "SCounter";
+		$this->version = $this->config['snippet']['version'] = '1.3';
+		$this->config['snippet']['versioncheck'] = "Unknown";
+		$this->client = evo()->getUserData();
 		$this->_check = 0;
 	}
-	
-	function Set($field, $value) {
+
+	function Set($field, $value)
+	{
 		$this->parameters[$field] = $value;
 	}
-	
-	function VersionCheck($version) {	
-		if ($version == $this->version) $this->_check = 1;
+
+	function VersionCheck($version)
+	{
+		if ($version == $this->version) {
+			$this->_check = 1;
+		}
 		$this->config["snippet"]["versioncheck"] = $version;
 	}
-	
-	function Run() {
-		global $modx;
-		if ($this->_check == 0)
+
+	function Run()
+	{
+		if ($this->_check == 0) {
 			return "Falsche Version";
+		}
 		$this->config["path"] = $this->parameters["path"];
 		$this->database->FirstRun($this->config["path"]);
-		
-		$return = $this->database->Read($this->parameters["minuten"]);
-		list($this->output["total"], $this->output["today"], $this->output["yesterday"], $this->output["useronline"]) = $return;
-		$this->setPlaceholders($this->output,"SCounter");
+
+        list(
+            $this->output["total"],
+            $this->output["today"],
+            $this->output["yesterday"],
+            $this->output["useronline"]
+            ) = $this->database->Read($this->parameters["minuten"]);
+		$this->setPlaceholders($this->output, "SCounter");
 	}
-	
-	function setPlaceholders($value = '', $key = '', $path = '') {
-		global $modx;
+
+	function setPlaceholders($value = '', $key = '', $path = '')
+	{
 		$keypath = !empty($path) ? $path . "." . $key : $key;
-	    $output = array();
-		if (is_array($value)) { 
+		if (is_array($value)) {
 			foreach ($value as $subkey => $subval) {
 				$this->setPlaceholders($subval, $subkey, $keypath);
-            }
+			}
 		} else {
-			if (strlen($this->config["tagid"]) > 0) {$keypath .= ".".$this->config["tagid"]; }
-			$modx->setPlaceholder($keypath,$value);	
+			if (strlen($this->config["tagid"]) > 0) {
+				$keypath .= "." . $this->config["tagid"];
+			}
+			evo()->setPlaceholder($keypath, $value);
 		}
 	}
-	
 }
-?>
